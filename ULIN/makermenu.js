91c3d6ba1d9af86e3f259803abafdda966a36eb2
UlinBOT12345 <79387080+UlinBOT12345@users.noreply.github.com>
2021-02-21 13:31:51 +0700

    Add files via upload

---
+++ ULIN/makermenu.js
@@ -0,0 +1,49 @@
+const makermenu = (prefix) => { 
+	return ` 
+╭───「 *CREATOR MENU* 」───
+│
+├➲ *${prefix}sticker*
+├➲ *${prefix}quotemaker [tx/wtrmk/tema]*
+├➲ *${prefix}nulis [nama/kelas/text]*
+├➲ *${prefix}trigger [reply image]*
+├➲ *${prefix}rip [reply image]*
+├➲ *${prefix}wasted [reply image]*
+├➲ *${prefix}cphlogo [UlinBot/Graphy]*
+├➲ *${prefix}cglitch [UlinBot/Graphy]*
+├➲ *${prefix}cpubg [UlinBot/Graphy]*
+├➲ *${prefix}cml [Salsa/UlinBot]*
+├➲ *${prefix}tahta [UlinBot]*
+├➲ *${prefix}croman [UlinBot dan Salsa]*
+├➲ *${prefix}cthunder [UlinBot]*
+├➲ *${prefix}cbpink [UlinBot]*
+├➲ *${prefix}cmwolf [UlinBot]*
+├➲ *${prefix}csky [UlinBot]*
+├➲ *${prefix}cwooden [UlinBot]*
+├➲ *${prefix}cflower [UlinBot]*
+├➲ *${prefix}clove [UlinBot]*
+├➲ *${prefix}ccrossfire [UlinBot]*
+├➲ *${prefix}cnaruto [UlinBot]*
+├➲ *${prefix}cparty [UlinBot]*
+├➲ *${prefix}cshadow [UlinBot]*
+├➲ *${prefix}cminion [UlinBot]*
+├➲ *${prefix}cneon [UlinBot]*
+├➲ *${prefix}cneon2 [UlinBot]*
+├➲ *${prefix}cneongreen [UlinBot]*
+├➲ *${prefix}c3d [UlinBot]*
+├➲ *${prefix}csky [UlinBot]*
+├➲ *${prefix}tts [id UlinBot]*
+├➲ *${prefix}ttp [UlinBot]*
+├➲ *${prefix}slide [UlinBot]*
+├➲ *${prefix}stiker*
+├➲ *${prefix}gifstiker*
+├➲ *${prefix}toimg*
+├➲ *${prefix}img2url*
+├➲ *${prefix}nobg*
+├➲ *${prefix}tomp3*
+├➲ *${prefix}ocr*
+│
+╰──────────────────────
+
+	           *©Ulin Gans*`
+	}
+exports.makermenu = makermenu
